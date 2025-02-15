3c0dbcb5845360fbe51325c8ccbca66308acd1b7
Degaga Emiru <degagaemiru@gmail.com>
2025-02-15 10:57:53 +0300

    new file added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
   import Footer from './components/Footer'
-
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +17,7 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <UserProfile name="Degaga" age="21" bio="Loves hiking and photography & watching film, coding" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
