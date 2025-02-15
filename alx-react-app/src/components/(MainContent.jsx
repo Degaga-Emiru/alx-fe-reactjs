fbc5264a9f7d6983b4be7c4f3689025b4dd90a1c
Degaga Emiru <degagaemiru@gmail.com>
2025-02-15 10:43:11 +0300

    added new file

---
+++ alx-react-app/src/components/(MainContent.jsx
@@ -0,0 +1,11 @@
+function Main(){
+    return(
+        <div>
+            <main>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+        </div>
+    );
+
+}
+ export default Main
