1d26ed5ac2045f98f6c7de27e16e8fe6af9c8d2d
Degaga Emiru <degagaemiru@gmail.com>
2025-03-02 10:22:07 +0300

    new file update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
@@ -12,7 +14,17 @@ function App() {
         <h1>Recipe Sharing App</h1>
         <SearchBar />
         <Routes>
-          <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+                <FavoritesList />
+                <RecommendationsList />
+              </>
+            }
+          />
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
         </Routes>
       </div>
