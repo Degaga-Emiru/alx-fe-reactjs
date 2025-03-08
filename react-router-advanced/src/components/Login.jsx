cac1c0156bfa89c1eaaeb109477dadf15e6f4502
Degaga Emiru <degagaemiru@gmail.com>
2025-03-08 15:16:14 +0300

    new project for advanced router

---
+++ react-router-advanced/src/components/Login.jsx
@@ -0,0 +1,21 @@
+// src/components/Login.jsx
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    localStorage.setItem("isAuthenticated", "true");
+    navigate("/profile");
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
