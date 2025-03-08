2b455291776e05e3e31a20eef7dd722a62566b61
Degaga Emiru <degagaemiru@gmail.com>
2025-03-08 21:10:50 +0300

    new file

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -5,12 +5,16 @@ import "@testing-library/jest-dom";
 
 // eslint-disable-next-line no-undef
 describe("TodoList Component", () => {
+  // eslint-disable-next-line no-undef
   test("renders initial todos", () => {
     render(<TodoList />);
+    // eslint-disable-next-line no-undef
     expect(screen.getByText("Learn React")).toBeInTheDocument();
+    // eslint-disable-next-line no-undef
     expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
   });
 
+  // eslint-disable-next-line no-undef
   test("adds a new todo", () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText("Add a new todo");
@@ -19,34 +23,43 @@ describe("TodoList Component", () => {
     fireEvent.change(input, { target: { value: "Test Todo" } });
     fireEvent.click(addButton);
 
+    // eslint-disable-next-line no-undef
     expect(screen.getByText("Test Todo")).toBeInTheDocument();
   });
 
+  // eslint-disable-next-line no-undef
   test("toggles a todo", () => {
     render(<TodoList />);
     const todoText = screen.getByText("Learn React");
 
     fireEvent.click(todoText);
+    // eslint-disable-next-line no-undef
     expect(todoText).toHaveStyle("text-decoration: line-through");
 
     fireEvent.click(todoText);
+    // eslint-disable-next-line no-undef
     expect(todoText).toHaveStyle("text-decoration: none");
   });
 
+  // eslint-disable-next-line no-undef
   test("deletes a todo", () => {
     render(<TodoList />);
     const deleteButton = screen.getAllByText("Delete")[0];
 
     fireEvent.click(deleteButton);
+    // eslint-disable-next-line no-undef
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 
+  // eslint-disable-next-line no-undef
   test("displays the number of remaining todos", () => {
     render(<TodoList />);
+    // eslint-disable-next-line no-undef
     expect(screen.getByText("2 todos remaining")).toBeInTheDocument();
 
     const todoText = screen.getByText("Learn React");
     fireEvent.click(todoText);
+    // eslint-disable-next-line no-undef
     expect(screen.getByText("1 todo remaining")).toBeInTheDocument();
   });
 });
