4368d139fb1288a7a78ae3fdbe109ead78f10060
Degaga Emiru <degagaemiru@gmail.com>
2025-03-12 21:16:32 +0300

    new file and feature

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -4,8 +4,8 @@ import UserProfile from './components/UserProfile';
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-6 bg-white rounded-2xl shadow-lg max-w-md">
+    <div className="flex items-center top-0 justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded-2xl shadow-lg max-w-md top-0">
         <p className="text-lg text-gray-700">
           This is a simple paragraph styled with Tailwind CSS in a React application.
         </p>
--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg
+      hover:shadow-xl transition-transform duration-300 ease-in-out">
       <img 
         src="src/img/programmer.jpeg" 
         alt="User" 
         className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center">John Doe</h1>
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center ">John Doe</h1>
       <p className="text-sm sm:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
