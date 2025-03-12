96fab66f2b3734a4759c389d74414ceca384c9ff
Degaga Emiru <degagaemiru@gmail.com>
2025-03-12 19:21:45 +0300

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -7,13 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  darkMode: 'class', // Or 'media' for system preference
   plugins: [],
-  variants: { // Only needed if you're adding custom variants
-    extend: {
-      backgroundColor: ['active'], // Example: Allow bg color to change on active state
-      opacity: ['disabled'], // Example: Allow opacity change on disabled state
-    },
-  },
-
 }
