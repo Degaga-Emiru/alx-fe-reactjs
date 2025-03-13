56f8aafa53e49c7707cd17ef7d6e42948ec1845a
Degaga Emiru <degagaemiru@gmail.com>
2025-03-13 07:36:45 +0300

    new feature

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,8 +9,8 @@ function UserProfile() {
         alt="User" 
         className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center ">Degaga Emiru</h1>
-      <p className="text-sm sm:text-base text-gray-600 text-center">
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center hover:text-blue-500 ">Degaga Emiru</h1>
+      <p className="text-sm sm:text-base text-gray-600 text-center hover:text-blue-500">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
