1c460563c25b46bfc6519fa0fbad92900759f2b5
Degaga Emiru <degagaemiru@gmail.com>
2025-03-13 07:34:51 +0300

    updated new file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,11 +5,11 @@ function UserProfile() {
     <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg
       hover:shadow-xl transition-transform duration-300 ease-in-out">
       <img 
-        src="src/img/programmer.jpeg" 
+        src="src/img/degooo.jpg" 
         alt="User" 
-        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto"
+        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center ">John Doe</h1>
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center ">Degaga Emiru</h1>
       <p className="text-sm sm:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
